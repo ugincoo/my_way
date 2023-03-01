05e471fcf8d4e6f274378c1ec607529f50fc1ffc
ujin <dbwl95@naver.com>
2023-03-02 08:06:21 +0900

    03/02 08:05 댓글삭제, front 글자 추가

--- Myway/src/myWay/Start.java
+++ Myway/src/myWay/Start.java
@@ -1,10 +1,12 @@
 package myWay;
 
+import myWay.view.Front;
 import myWay.view.MemberFront;
 
 
 public class Start {
 	public static void main(String[] args) {
+		Front.getInstance().hi();
 		MemberFront.getInstance().index();
 	}
 }
