cf1991f11e1f256c3baafbc1292b048d93dd8d26
kimy041 <miyeong0418@gmail.com>
2023-03-02 18:05:39 +0900

    03/02_18:05

--- Myway/src/myWay/controller/SalesController.java
+++ Myway/src/myWay/controller/SalesController.java
@@ -2,6 +2,7 @@ package myWay.controller;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Scanner;
 
 import myWay.dao.SalesDao;
 import myWay.dto.SalesDto;
@@ -19,39 +20,40 @@ public class SalesController {
 	int year = cal.get( Calendar.YEAR );
 	int month = cal.get( Calendar.MONTH )+1;
 	int day = cal.get( Calendar.DAY_OF_MONTH );
-		
+	
 	// 달력 출력
 	public void Calendar( int choice ) {
 		
-		while(true) {
-			
-			System.out.printf("=================== %d 년 %d 월 ===================\n" , year , month );
-			System.out.println("일\t월\t화\t수\t목\t금\t토");
-			cal.set(year, month-1, 1); //  현재 연도/월 의 1일 날짜 형식으로 변경
-			int sweek = cal.get( Calendar.DAY_OF_WEEK ); // 해당 월의 시작일
-			int eday = cal.getActualMaximum( Calendar.DAY_OF_MONTH ); // 해당 월의 마지막일
-			
-			for( int i = 1; i<sweek ; i++ ) { System.out.print("\t"); } // 해당 월 1일 전까지 공백으로 채우기
-			
-			for( int i = 1; i<= eday ; i++ ) {
-				System.out.printf("%2d\t" , i);
-				if( sweek%7 == 0 ) { System.out.println(); }
-				sweek++;
-				
-			}
-			System.out.println("\n===================================================");
-			if( choice == -1 ) {
-				month--;
-				if( month < 1 ) { month = 12; year--; }
-			}
-			else if( choice == 1 ) {
-				month++;
-				if( month > 12 ) { month = 1; year++; }
-			}
-			return;
+		if( choice == -1 ) {
+			month--;
+			if( month < 1 ) { month = 12; year--; }
+		}
+		else if( choice == 1 ) {
+			month++;
+			if( month > 12 ) { month = 1; year++; }
 		}
 		
-	}
+		System.out.printf("=================== %d 년 %d 월 ===================\n" , year , month );
+		System.out.println("\t일\t\t월\t\t화\t\t수\t\t목\t\t금\t\t토\t");
+		cal.set(year, month-1, 1); //  현재 연도/월 의 1일 날짜 형식으로 변경
+		int sweek = cal.get( Calendar.DAY_OF_WEEK ); // 해당 월의 시작일
+		int eday = cal.getActualMaximum( Calendar.DAY_OF_MONTH ); // 해당 월의 마지막일
+		
+		for( int i = 1; i<sweek ; i++ ) { System.out.print("\t\t"); } // 해당 월 1일 전까지 공백으로 채우기
+		int line = 1;
+		for( int i = 1; i<= eday*2 ; i++ ) {
+			if(line%2 == 1) {
+				System.out.printf("\t%2d\t" , i);
+			}else {
+				System.out.printf("\t%2d\t" , findPrice(year+"-"+month+"-"+i));
+			}
+			if( sweek%7 == 0 ) { System.out.println(); line++; }
+			sweek++;
+			
+		}
+		
+		System.out.println("\n===================================================");
+		}
 	
 	
 	// 월 매출 출력 or 일 매출 출력
@@ -59,4 +61,7 @@ public class SalesController {
 		return SalesDao.getInstance().printSales();
 	}
 	
+	public int findPrice(String date) {
+		
+	}
 }
--- Myway/src/myWay/view/StockFront.java
+++ Myway/src/myWay/view/StockFront.java
@@ -128,26 +128,28 @@ public class StockFront {
 	
 	//매출현황
 	public void printCurrentSales() {
-		
-		//달력 출력 하고!
-		System.out.println();
-		SalesController.getInstance().Calendar( 0 );
-		System.out.println("[-1]이전달 [0]월 매출보기 [1]다음달 [2]일 매출보기 [3]나가기 : ");
-		int choice = scanner.nextInt();
-		if(choice == -1){ // 이전달
-			System.err.println();
-			SalesController.getInstance().Calendar( choice );
-		}else if(choice == 0) { //달력에 보여진 월의 매출
-			SalesFront.getInstance().printCurrentSales("null");
-		}else if(choice == 1) {
+		while(true) {
+			//달력 출력 하고!
 			System.out.println();
-			SalesController.getInstance().Calendar( choice );
-		}else if(choice == 2) {
-			System.out.print("일 입력 : ");
-			int selectDay = scanner.nextInt();
-			SalesFront.getInstance().printCurrentSales("null");
-		}else if(choice == 3) {
-			return;
+			SalesController.getInstance().Calendar( 0 );
+			System.out.println("[-1]이전달 [0]월 매출보기 [1]다음달 [2]일 매출보기 [3]나가기 : ");
+			int choice = scanner.nextInt();
+			
+			if(choice == -1){ // 이전달
+				System.err.println();
+				SalesController.getInstance().Calendar( choice );
+			}else if(choice == 0) { //달력에 보여진 월의 매출
+				SalesFront.getInstance().printCurrentSales("null");
+			}else if(choice == 1) {
+				System.out.println();
+				SalesController.getInstance().Calendar( choice );
+			}else if(choice == 2) {
+				System.out.print("일 입력 : ");
+				int selectDay = scanner.nextInt();
+				SalesFront.getInstance().printCurrentSales("null");
+			}else if(choice == 3) {
+				return;
+			}
 		}
 	}
 	
